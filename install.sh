cbca1e4dec11ea9f7d1de5eddaa27d3879b3c54e
Vincent Spiewak <vspiewak@gmail.com>
2019-07-21 01:08:35 +0200

    Add check boot mode

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+EFI_VARS_FILE=/sys/firmware/efi/efivars
+
 echo "Installing Arch"
+
+# check boot mode
+if test -f "${EFI_VARS_FILE}"; then
+  echo "Boot Mode: UEFI"
+else
+  echo "Boot Mode: BIOS"
+fi
